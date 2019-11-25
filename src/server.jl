931441a7ed1cdd8c65cb984fcb0120b475368e55
Dominic Morgan <dominic.mark.morgan@gmail.com>
2019-11-25 10:35:54 +0800

    add web socket server

--- src/server.jl
+++ src/server.jl
@@ -30,11 +30,45 @@ function create_response(data :: Any)
     response
 end
 
-function start(router; port=8081, four_oh_four = x -> "404")
-    @info "Starting server on port: $port"
-    server = Sockets.listen(port)
 
-    @async HTTP.serve(Sockets.localhost, port; server=server) do request
+function check_server_started(server, task::Task, timeout=3.0)
+
+    task_failed = timedwait(() -> !istaskdone(task), float(timeout))
+
+    if task_failed == :timed_out
+        close(server)
+        rethrow(task.exception)
+    end;
+end
+
+function ws_serve(f :: Function;
+    port = 8081,
+    binary=false, verbose=false, timeout=3
+    )
+
+    server = Sockets.listen(UInt16(port))
+
+    task = @async HTTP.listen(Sockets.localhost, port; server=server, verbose=verbose) do http
+        HTTP.WebSockets.upgrade(f, http; binary=binary)
+    end
+
+    check_server_started(server, task)
+    server
+end
+
+
+function http_serve(f :: Function; port = 8081, timeout=3.0)
+    server = Sockets.listen(UInt16(port))
+    task = @async HTTP.serve(f, Sockets.localhost, port; server=server) 
+    check_server_started(server, task, timeout)
+    server
+end
+
+
+function start(router; http_port = 8081, ws_port = http_port + 1, four_oh_four = x -> "404")
+    @info "Starting server on port: $http_port"
+
+    http_server = http_serve() do request
         trie = router.routes[request.method]
         uri =  URI(request.target)
         handler, route = get_handler(trie, String(uri.path), four_oh_four)
@@ -43,10 +77,28 @@ function start(router; port=8081, four_oh_four = x -> "404")
         context |> handler |> create_response
     end
 
-    #For some reason this request is needed to update Routes in the sever?
-    @assert HTTP.get("http://localhost:$port/").status == 200
+    #For some reason this request is needed to update Routes in the sever
+    @assert HTTP.get("http://localhost:$http_port/").status == 200
 
-    server
+    # TODO if you want to start just a http or just  ws server
+    # Should split into ws_start and HTTP start that just take a router
+    ws_server = ws_serve(port=ws_port) do ws
+        while !eof(ws)
+            data = readavailable(ws)
+            write(ws, data)
+        end
+    end
+
+
+    Server(http_server, ws_server)
 end
 
-stop(server) = close(server)
+struct Server
+    http_server
+    ws_server
+end
+
+function stop(server::Server) 
+    close(server.http_server)
+    close(server.ws_server)
+end
