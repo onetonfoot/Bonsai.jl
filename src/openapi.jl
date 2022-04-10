7a97e700c174e8cf231c7f2f4435c422ba0a3deb
dom <onetonfoot@gmail.com>
2022-04-10 11:50:09 +0800

    add remaining types

--- src/openapi.jl
+++ src/openapi.jl
@@ -1,30 +1,30 @@
 # https://swagger.io/specification/
-module Swagger
+module OpenAPIv3
 
-@enum In query header path cookie
-
-struct Parameter
-	name::String
-	required::Bool
-	in::In
+struct ExternalDocumentationObject
 	description::String
-	depecated::Bool
-	allowEmptyValue::Bool
-
-	explode::Bool
-	allowReserved::Bool
-	schema::Schema
+	url::String
 end
 
-struct Contact
+struct TagObject
 	name::String
-	url::String
-	email::String
+	description::String
+	externalDocs::ExternalDocumentationObject
 end
 
-struct License 
-	name::String
-	url::String
+struct DiscriminatorObject
+	propertyName::String
+	mapping::Dict{String, String}
+end
+
+struct SchemaObject
+	nullable::Bool
+	discriminator::DiscriminatorObject
+	readOnly::Bool
+	writeOnly::Bool
+	externalDocs::ExternalDocumentationObject
+	example::Any
+	deprecated::Bool
 end
 
 struct ServerVariable
@@ -33,16 +33,139 @@ struct ServerVariable
 	description::String
 end
 
-struct Components
-	schema::Dict{String, Union{Schema, Object, ReferenceObject}}
-	responses::Dict{String, Response, Object, ReferenceObject}
-	parameters::Dict{String, Response, Object, ReferenceObject}
-	examples::Dict{String, Example, Object, ReferenceObject}
-	requestBodies::Dict{String, Union{Request, BodyObject, ReferenceObject}}
-	securitySchemes::Dict{String, SecuritySchemeObject, ReferenceObject}
-	links::Dict{String, LinkObject, ReferenceObject}
-	callbacks::Dict{String, CallbackObject, ReferenceObject}
+struct ServerObject
+	url::String
+	description::String
+	variables::Dict{String, ServerVariable}
 end
+
+struct HeaderObject
+	description::String
+	required::Bool
+	depecated::Bool
+	allowEmptyValue::Bool
+	style::String
+	explode::Bool
+	allowReserved::Bool
+end
+
+struct LinkObject
+	operationRef::String
+	operationId::String
+	parameters::Dict{String, String}
+	requestBody::String
+	description::String
+	server::ServerObject
+end
+
+struct Contact
+	name::String
+	url::String
+	email::String
+end
+
+
+struct ExampleObject
+	summary::String
+	description::String
+	# can have either value or external value not both
+	value::Any
+	externalValue::String
+end
+
+
+struct EncodingObject
+	contentType::String
+	headers::Dict{String, HeaderObject}
+	styled::String
+	explode::Bool
+	allowReserved::Bool
+end
+
+struct MediaTypeObject
+	schema::SchemaObject
+	example::Any
+	examples::Dict{String, ExampleObject}
+	encoding::Dict{String, EncodingObject}
+end
+
+
+@enum In query header path cookie
+
+struct ParameterObject
+	name::String
+	in::In
+	description::String
+	required::Bool
+	depecated::Bool
+	allowEmptyValue::Bool
+	style::String
+	explode::Bool
+	allowReserved::Bool
+	schema::SchemaObject
+	example::Any
+	examples::Dict{String, ExampleObject}
+	content::Dict{String, MediaTypeObject}
+end
+
+
+struct OAuthFlowObject
+	authorizationUrl::String
+	tokenUrl::String
+	refreshUrl::String
+	scopes::Dict{String, String}
+end
+
+struct SecurityRequirementObject
+	# custom serialization
+	requirements::Dict{String, Array{String}}
+end
+
+
+
+struct License 
+	name::String
+	url::String
+end
+
+
+struct RequestBodyObject
+	description::String
+	content::Dict{String, MediaTypeObject}
+	required::Bool
+end
+
+
+struct ResponseObject
+	description::String
+	headers::Dict{String, HeaderObject}
+	content::String
+	links::Dict{String, LinkObject}
+end
+
+struct ResponsesObject
+	default::ResponseObject
+	# needs custom serialization
+	statusCodes::Dict{String, ResponseObject}
+end
+
+
+# note to self start from here
+struct OperationObject
+	tags::Array{String}
+	summary::Union{String, Nothing}
+	description::Union{String, Nothing}
+	externalDocs::Union{ExternalDocumentationObject, Nothing}
+	operationId::String
+	parameters::Array{ParameterObject}
+	requestBody::Union{RequestBodyObject, Nothing}
+	responses::Union{ResponseObject, Nothing}
+	callbacks::Dict{String, Dict{String, Union{String, Array{String}}}}
+	depercated::Bool
+	security::Array{SecurityRequirementObject}
+	servers::Array{ServerObject}
+end
+
 
 struct PathItemObject
 	summary::Union{String, Nothing}
@@ -55,43 +178,35 @@ struct PathItemObject
 	head::Union{String, OperationObject}
 	patch::Union{String, OperationObject}
 	trace::Union{String, OperationObject}
-	servers::Array{SeversObject}
-	parameters::Union{ParametersObject, Nothing}
+	servers::Array{ServerObject}
+	parameters::Union{ParameterObject, Nothing}
+end
+
+struct CallbackObject
+	# needs custom serialization
+	callbacks::Dict{String, PathItemObject}
 end
 
 
-# note to self start from here
-struct OperationObject
-	tags::Array{String}
-	summary::Union{String, Nothing}
-	description::Union{String, Nothing}
-	externalDocs::Union{ExternalDocumentaionObject, Nothing}
-	operationId::String
-	parameters::Array{ParamterObject}
-	requestBody::Union{RequestBodyObject, Nothing}
-	responses::Union{ResponseObject, Nothing}
+struct OAuthFlowsObject
+	implict::OAuthFlowObject
+	password::OAuthFlowObject
+	clientCredntials::OAuthFlowObject
+	authorizationCode::OAuthFlowObject
+end
+
+struct SecuritySchemeObject
+	type::String
+	description::String
+	name::String
+	in::String
+	scheme::String
+	bearerFormatString
+	flows::OAuthFlowsObject
+	openIdConnectUrl::String
 end
 
 const PathsObject = Pair{String, PathItemObject} 
-
-
-struct Sever 
-	url::String
-	description::String
-	variables::Dict{String, ServerVariable}
-end
-
-struct OpenAPI
-	openapi::String # semantic version number
-	info::Union{Info, Nothing}
-	servers::Array{Servers}
-	paths::Union{Paths, Nothing}
-	components::Union{Components, Nothing}
-	security::Array{Security}
-	tags::Array{Tags}
-	externalDocs::Union{ExternalDocs, Nothing}
-end
-
 
 struct Info 
 	title::String 
@@ -100,6 +215,28 @@ struct Info
 	contact::Union{Contact, Nothing}
 	license::Union{License, Nothing}
 	version::String  
+end
+
+struct Components
+	schema::Dict{String, Union{SchemaObject}}
+	responses::Dict{String, ResponseObject}
+	parameters::Dict{String, ResponseObject}
+	examples::Dict{String, ExampleObject}
+	requestBodies::Dict{String, RequestBodyObject}
+	securitySchemes::Dict{String, SecuritySchemeObject}
+	links::Dict{String, LinkObject}
+	callbacks::Dict{String, CallbackObject}
+end
+
+struct OpenAPI
+	openapi::String # semantic version number
+	info::Union{Info, Nothing}
+	servers::Array{ServerObject}
+	paths::Union{PathsObject, Nothing}
+	components::Union{Components, Nothing}
+	security::Array{SecurityRequirementObject}
+	tags::Array{TagObject}
+	externalDocs::Union{ExternalDocumentationObject, Nothing}
 end
 
 end
