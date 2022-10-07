c801d695b7c9df58198543d1efe76b6433f093bf
dom <onetonfoot@gmail.com>
2022-10-07 08:31:43 +0800

    remove type check which doesn't work correctly

--- src/handlers.jl
+++ src/handlers.jl
@@ -1,10 +1,7 @@
-import FilePathsBase: /
-using Base.Libc
-
 """
 Has a field fn which takes stream as it's first argument
 
- fn(stream, ...)
+fn(stream, ...)
 """
 abstract type AbstractHandler end
 
@@ -13,9 +10,6 @@ abstract type AbstractHandler end
 mutable struct Middleware  <: AbstractHandler 
     fn 
     function Middleware(fn)
-        if isnothing(safe_which(fn, Tuple{Any, Any}))
-            error("Invalid function signature must match fn(::Stream, next::Any)")
-        end
         new(fn)
     end
 end
@@ -24,17 +18,6 @@ mutable struct HttpHandler  <: AbstractHandler
 	fn
 
     function HttpHandler(fn)
-        if isnothing(safe_which(fn, Tuple{Any}))
-            error("Invalid function signature must match fn(::Stream)")
-        end
         new(fn)
-    end
-end
-
-function safe_which(fn, args)
-    try
-        which(fn, args)
-    catch
-        nothing
     end
 end
