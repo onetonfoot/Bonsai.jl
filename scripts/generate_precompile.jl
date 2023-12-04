626d8cdba4439c59ed4a808cc360a6cad301cd9c
dom <onetonfoot@gmail.com>
2023-12-04 17:49:23 +0800

    stricter JET version

--- scripts/generate_precompile.jl
+++ scripts/generate_precompile.jl
@@ -1,8 +1,7 @@
-# generate_precompile.jl
 using SnoopCompile, Bonsai, ProfileView
 using Bonsai.HTTP, Bonsai.StructTypes, Bonsai.JSON3
 
-function run_my_project()
+function run_test_suite()
 
     paths = [
         joinpath(@__DIR__, "../test/app.jl"),
@@ -14,39 +13,11 @@ function run_my_project()
         joinpath(@__DIR__, "../test/utils.jl"),
     ]
 
-    #=
-        ideally we would walk the expression and transform using
-        import statments from StructTypes to Bonsai.StructTypes
-        we could then use the test suite to generate precompile
-        statements without modifying the test src. But for now 
-        well just to the dump thing a modify the src cos I cba
-        to get into ast right now.
-
-        If the approach works well it could be put in a package
-    =#
-
     for path in paths
         include(path)
     end
 end
 
-tinf = @snoopi_deep run_my_project()
-# tinf = @snoopi_deep include(joinpath(@__DIR__, "/test/app.jl"))
-
-# ProfileView.@profview flamegraph(tinf)
-
-# show(plot)
+tinf = @snoopi_deep run_test_suite()
 ttot, pcs = SnoopCompile.parcel(tinf);
 SnoopCompile.write(joinpath(@__DIR__, "../src/precompile"), pcs)
-
-
-# ProfileView.@profview plot
-
-
-# using Base.Meta
-
-# using StructTypes
-# using StructTypes, JSON3
-
-# import StructTypes: @Struct
-# import StructTypes: @Struct, SomeOtherThing
